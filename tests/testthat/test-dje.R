c8c6b6c2bc1329541b1f0406c797088ffc32c085
Caio Lente <ctlente@gmail.com>
2017-08-07 11:37:37 -0300

    Adapted tests to new code

--- tests/testthat/test-dje.R
+++ tests/testthat/test-dje.R
@@ -1,24 +1,14 @@
+library(dje)
 context("dje")
 
-n_arqs <- tibble::tibble(
-  tj = c('TJSP'),
-  n_arqs = c(6)
-)
+test_that("download_dje() is able to download a DJE from SP", {
 
-expect_dje <- function(tj = 'TJSP', date = "2017-08-07") {
-  download_dje(tj = tj, dates = date, path = '.')
-  path <- sprintf('%s_dje_%s', tolower(tj), date)
-  arqs <- dir(path, full.names = TRUE)
-  pdfs <- sapply(arqs, readr::read_lines, n_max = 1L)
-  v <- n_arqs[n_arqs$tj == tj,][['n_arqs']]
-  val <- all(stringr::str_detect(pdfs, 'PDF')) && length(arqs) == v
-  expect(val, 'Nao tem o numero certo de arquivos ou nao sao PDF.')
-}
+  # Download DJE
+  res <- download_dje("TJSP", "2017-08-07", tempdir())
 
-test_that("baixa tjsp de hoje", {
-  tj <- 'TJSP'
-  date <- Sys.Date()
-  exp <- expect_dje(tj, date)
-  unlink(sprintf('%s_dje_%s', tolower(tj), date), recursive = TRUE)
-  invisible(exp)
+  # Tests
+  expect_length(res$file, 6)
+  expect_true(all(res$date == "2017-08-07"))
+  expect_equal(res$booklet, c(11:15, 18))
+  expect_true(all(res$result == "OK"))
 })
