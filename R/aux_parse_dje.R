5dd56962d6ee74a6e3a2e09b8aa41bcfb0d98a84
Fernando <fptcorrea@gmail.com>
2018-01-02 17:34:17 -0200

    auxiliar functions for parsing documents

---
+++ R/aux_parse_dje.R
@@ -0,0 +1,50 @@
+header_style <- function(date = 'Publicação: [a-zç]+-feira, [0-9]+ de [a-z]+ de 201[0-9]',
+             document_type = 'Diário da Justiça Eletrônico - Caderno Judicial - [0-9]ª Instância',
+             location = 'Campo Grande, Ano [XVI]+ - Edição [0-9]+',
+             separation = "\n\n"){
+  str_c(c(date, document_type, location), collapse = separation)
+}
+
+footer_style <- function(){
+  'Publicação Oficial do Tribunal de Justiça do Estado de Mato Grosso do Sul - Lei Federal nº 11.419/06, art. 4º.'
+}
+
+break_text <- function(text_chr, patterns){
+  positions <- purrr::map_int(patterns, regexpr, text = text_chr)
+  names(positions) <- patterns
+  sort(positions[positions > 0])
+}
+
+remove_header <- function(text_chr, header_regex = header_style()){
+  first_pattern <- stringr::str_extract(text_chr, header_regex)
+  clean_text <- stringr::str_replace_all(texto, first_pattern, "")
+}
+
+remove_footer <- function(text_chr, footer_regex = footer_style()){
+  clean_text <- stringr::str_replace_all(texto, footer_regex, "")
+}
+
+find_index <- function(text_chr, index_regex = "\nSUMÁRIO\n"){
+  position <- regexpr(index_regex, text_chr)
+
+  summary_chr <- stringr::str_sub(text_chr, position[1], -1)
+}
+
+break_chunk <- function(txt_chunk){
+  txt_chunk %>%
+    stringr::str_split(paste0("(?=(\nProcesso ", cnj_format, "))"))
+}
+
+parse_docket_entry <- function(raw_docket_entry){
+  raw_docket_entry %>%
+    stringr::str_split(pattern = "(?=\n(Herdeir[oa]:|Impugd[oa]:|Exqte:|Credor Hip:|Infratora?:|A\\. Fato:|Alimtd[oa]:|Alimtte:|Réu?:|Indiciad[ao]:|Exectd[ao]:|Reclamte:|Imptte:|Embargte:|Exeqte:|Reqte:|Impugte:|Denunciad[ao]:|Menor:|Embargd[ao]:|Re[qp]d[oa]:|Agravd[ao]:|Querelada:|Cessionári[ao]?:|Recorrente:|Reclamd[ao]:|Adotante:|Interesd[oa]\\.?:|Opte:|Invtante:|Autora?:))") %>%
+    dplyr::first() %>%
+    {if(length(.) != 2){
+      c(paste(., collapse = ""), NA)
+    } else {
+      .
+    }} %>%
+    purrr::set_names(c("lawsuit_id", "docket")) %>%
+    as.list()
+}
+
