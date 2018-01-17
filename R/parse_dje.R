ee9d0aa77a3b6bc8ad31d0c1c3fa678a272539c1
Julio Trecenti <julio.trecenti@gmail.com>
2018-01-17 00:08:54 -0200

    added refs, made it check

--- R/parse_dje.R
+++ R/parse_dje.R
@@ -1,14 +1,16 @@
 parse_dje_tjms <- function(text_file){
-  clean_text <- text_file %>%
+  txt <- readr::read_file(text_file)
+
+  clean_text <- txt %>%
     remove_header() %>%
     remove_footer()
 
   sumario <- clean_text %>%
     find_index() %>%
-    str_split("(SUMÁRIO|\b)\n") %>%
-    first() %>%
-    str_subset("Juizado|Vara") %>%
-    str_trim()
+    stringr::str_split("(SUM\u00c1RIO|\b)\n") %>%
+    dplyr::first() %>%
+    stringr::str_subset("Juizado|Vara") %>%
+    stringr::str_trim()
 
   courts <- sumario %>%
     paste0("\n", ., "\n") %>%
@@ -16,111 +18,117 @@ parse_dje_tjms <- function(text_file){
     append(-1)
 
   d <- purrr::map(1:(length(courts)-1),
-                  ~str_sub(clean_text, courts[.x], courts[.x+1])) %>%
+                  ~stringr::str_sub(clean_text, courts[.x], courts[.x+1])) %>%
     purrr::map(~break_chunk(.x) %>%
-                 first() %>%
+                 dplyr::first() %>%
                  purrr::map_df(parse_docket_entry)) %>%
-    purrr::map2(names(courts)[-length(courts)], ~mutate(.x, location = .y)) %>%
-    bind_rows() %>%
-    filter(str_detect(lawsuit_id, "Processo"))
+    purrr::map2(names(courts)[-length(courts)],
+                ~dplyr::mutate(.x, location = .y)) %>%
+    dplyr::bind_rows() %>%
+    dplyr::filter(stringr::str_detect(lawsuit_id, "Processo"))
 }
 
-parse_dje_tjsp <- function(text_file){
+
+#' Parse DJE file
+#'
+#' parse TJSP file obtained from [dje_to_text()].
+#'
+#' @param text_file path of the file to parse.
+#'
+#' @return tibble containing lawsuit and court information
+#' @export
+parse_dje_tjsp <- function(text_file) {
 
   # clean_text <- text_file %>%
   #   remove_header() %>%
   #   remove_footer()
+  txt <- readr::read_file(text_file)
+  cnj_format_sp <- stringr::regex("[0-9]{7}\\-[0-9]{2}\\.[0-9]{4}\\.8\\.26\\.[0-9]{4}")
+  clean_text <- paste0(txt, "@fim_do_texto@")
 
-  clean_text <- paste0(text_file, "@fim_do_texto@")
-
-  classify_content <- function(raw_content){
-    case_when(
-      str_detect(raw_content, "Distribuidor") ~ "D",
-      str_detect(raw_content, "[XVI ]+-|Fórum|^[ÇA-ZÃÂÁÍÓÚÉÊ ]+$") ~ "C",
+  classify_content <- function(raw_content) {
+    dplyr::case_when(
+      stringr::str_detect(raw_content, "Distribuidor") ~ "D",
+      stringr::str_detect(raw_content, "[XVI ]+-|F\u00f3rum|^[\u00c7A-Z\u00c3\u00c2\u00c1\u00cd\u00d3\u00da\u00c9\u00ca ]+$") ~ "C",
       TRUE ~ "Outros")
   }
 
   index <- clean_text %>%
-    find_index(index_regex = "\nSUMÁRIO\nTRIBUNAL") %>%
-    str_split("SUMÁRIO|\n") %>%
-    first() %>%
-    str_trim() %>%
-    str_subset("^[XVI ]+-|Fórum|^[ÇA-ZÃÂÁÍÓÚÉÊ -]+$|Vara|Anexo|Distribuidor|Juizado|Fiscais|Criminal|Cível") %>%
-    as.tibble() %>%
-    set_names("valor") %>%
-    mutate(tipo = classify_content(valor))
+    find_index(index_regex = "\nSUM\u00c1RIO\nTRIBUNAL") %>%
+    stringr::str_split("SUM\u00c1RIO|\n") %>%
+    dplyr::first() %>%
+    stringr::str_trim() %>%
+    stringr::str_subset("^[XVI ]+-|F\u00f3rum|^[\u00c7A-Z\u00c3\u00c2\u00c1\u00cd\u00d3\u00da\u00c9\u00ca -]+$|Vara|Anexo|Distribuidor|Juizado|Fiscais|Criminal|C\u00edvel") %>%
+    tibble::as.tibble() %>%
+    purrr::set_names("valor") %>%
+    dplyr::mutate(tipo = classify_content(valor))
 
-  funcao <- function(x, y){
-    if(str_detect(x[[1]][length(x[[1]])], "Distribuidor")){
-      x <- bind_rows(x, tibble(valor = ifelse(is.na(y), "@fim_do_texto@", y)))
+  clean_breaks <- function(x, y) {
+    if (stringr::str_detect(x[[1]][length(x[[1]])], "Distribuidor")) {
+      tab2 <- tibble::tibble(valor = ifelse(is.na(y), "@fim_do_texto@", y))
+      x <- dplyr::bind_rows(x, tab2)
     } else {
       x
     }
   }
 
   breaks <- index %>%
-    filter((tipo == "D")|(lag(tipo) == "D")|(tipo == "C" & lead(tipo) != "C")) %>%
-    filter(is.na(lead(tipo))|!(tipo == "C" & lead(tipo) == "C")) %>%
-    mutate(classe = ifelse(tipo == "C" & !is.na(lead(tipo)), valor, NA),
-           valor = paste0("\n", valor, "\n")) %>%
-    fill(classe) %>%
-    mutate(classe = paste0("\n", classe, "\n")) %>%
-    select(classe, valor) %>%
-    group_by(classe) %>%
-    nest(.key = "valor") %>%
-    mutate(valor = purrr::map2(valor, lead(classe), funcao))
+    dplyr::filter((tipo == "D") |
+                    (dplyr::lag(tipo) == "D") |
+                    (tipo == "C" & dplyr::lead(tipo) != "C")) %>%
+    dplyr::filter(is.na(dplyr::lead(tipo)) |
+                    !(tipo == "C" & dplyr::lead(tipo) == "C")) %>%
+    dplyr::mutate(classe = ifelse(tipo == "C" & !is.na(dplyr::lead(tipo)),
+                                  valor, NA),
+                  valor = paste0("\n", valor, "\n")) %>%
+    tidyr::fill(classe) %>%
+    dplyr::mutate(classe = paste0("\n", classe, "\n")) %>%
+    dplyr::select(classe, valor) %>%
+    dplyr::group_by(classe) %>%
+    tidyr::nest(.key = "valor") %>%
+    dplyr::mutate(valor = purrr::map2(valor, dplyr::lead(classe), clean_breaks))
 
   breaks_counties <- clean_text %>%
     break_text(breaks$classe) %>%
     append(-1)
 
-  cut_text <- function(x,y){
+  cut_text <- function(x, y) {
 
-    if(breaks_counties[x+1] == -1){
+    if (breaks_counties[x+1] == -1){
       text <- clean_text %>%
-        str_sub(breaks_counties[x], breaks_counties[x+1])
+        stringr::str_sub(breaks_counties[x], breaks_counties[x+1])
     } else {
       text <- clean_text %>%
-        str_sub(breaks_counties[x], breaks_counties[x+1] + nchar(breaks_counties[x+1])-1)
+        stringr::str_sub(breaks_counties[x],
+                         breaks_counties[x + 1] + nchar(breaks_counties[x + 1]) - 1)
     }
 
-    print(y$valor[1])
-    if(length(y$valor) == 3){
-
+    # print(y$valor[1])
+    if(length(y$valor) == 3) {
       lim_inf <- text %>%
         break_text(y$valor[2])
-
       points <- text %>%
-        str_sub(lim_inf, -1) %>%
+        stringr::str_sub(lim_inf, -1) %>%
         break_text(y$valor[2:3])
-
-      str_sub(str_sub(text, lim_inf, -1), points1[1], points1[2])
+      r <- stringr::str_sub(stringr::str_sub(text, lim_inf, -1), points[1], points[2])
     } else {
-
-      distribuidor <- which(str_detect(y$valor, "Distribuidor"))
-
+      distribuidor <- which(stringr::str_detect(y$valor, "Distribuidor"))
       r <- character(length(distribuidor))
-
-      for(i in 1:length(distribuidor)){
+      for(i in 1:length(distribuidor)) {
         if(i == 1){
           points <- text %>%
-            break_text(y$valor[distribuidor[1]:(distribuidor[1]+1)])
-
-          r[i] <- str_sub(text, points[1], points[2])
+            break_text(y$valor[distribuidor[1]:(distribuidor[1] + 1)])
+          r[i] <- stringr::str_sub(text, points[1], points[2])
         } else {
           lim_inf <- text %>%
             break_text(y$valor[distribuidor[i]])
-
           points <- text %>%
-            str_sub(lim_inf, -1) %>%
+            stringr::str_sub(lim_inf, -1) %>%
             break_text(y$valor[distribuidor[i]:(distribuidor[i]+1)])
-
-          r[i] <- str_sub(str_sub(text, lim_inf, -1), points[1], points[2])
-
+          r[i] <- stringr::str_sub(stringr::str_sub(text, lim_inf, -1),
+                                   points[1], points[2])
         }
       }
-
-      r
       # points1 <- text %>%
       #   break_text(y$valor[distribuidor[1]:(distribuidor[1]+1)])
       #
@@ -128,22 +136,23 @@ parse_dje_tjsp <- function(text_file){
       #   break_text(y$valor[distribuidor[2]])
       #
       # points2 <- text %>%
-      #   str_sub(lim_inf, -1) %>%
+      #   stringr::str_sub(lim_inf, -1) %>%
       #   break_text(y$valor[distribuidor[2]:(distribuidor[2]+1)])
       #
-      # c(str_sub(text, points1[1], points1[2]),
-      #   str_sub(str_sub(text, lim_inf, -1), points2[1], points2[2]))
+      # c(stringr::str_sub(text, points1[1], points1[2]),
+      #   stringr::str_sub(stringr::str_sub(text, lim_inf, -1), points2[1], points2[2]))
+      r
     }
   }
 
-  inner_breaks <- purrr::map2(1:length(breaks$classe), breaks$valor,
-                              cut_text) %>%
+  inner_breaks <- purrr::map2(seq_along(breaks$classe), breaks$valor, cut_text) %>%
     unlist() %>%
-    str_split(pattern = paste0("(?=PROCESSO ?:", cnj_format_sp, ")"))
+    stringr::str_split(pattern = paste0("(?=PROCESSO ?:", cnj_format_sp, ")"))
 
   d <- breaks %>%
-    unnest(valor) %>%
-    filter(str_detect(valor, "Distribuidor"))  %>%
-    mutate(processos = inner_breaks) %>%
-    unnest(processos)
+    tidyr::unnest(valor) %>%
+    dplyr::filter(stringr::str_detect(valor, "Distribuidor"))  %>%
+    dplyr::mutate(processos = inner_breaks) %>%
+    tidyr::unnest(processos)
+  d
 }
